998db7574a99b07778d5719ac4271b3d60923765
harshparihar6868@gmail.com <harshparihar6868@gmail.com>
2026-09-05 23:05:48 +0530

    add semantic

---
+++ HTML/Lab_Folder/list.py
@@ -0,0 +1,11 @@
+j =[1, 2, 345, [4,5],]
+
+# Sorting
+
+"""
+1. sort() -> None => list method => sorts list in ascending order by default
+2. sorted(list_name, reverse=False, key=None) -> List => direct built methods => sortsa list in ascending order by default and provides neew list in return without changing original list.
+"""
+
+
+""
